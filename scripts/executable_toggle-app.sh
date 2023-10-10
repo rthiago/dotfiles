d23ca3bc054a2965a5614e2769d597f7c746b430
Thiago Silva <rthiago@gmail.com>
2023-10-10 17:27:23 -0300

    Improve toggle script

--- scripts/executable_toggle-app.sh
+++ scripts/executable_toggle-app.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <window_class> <launch_command>"
+# Example:
+# /home/thiago/scripts/toggle-app.sh "Subl" "subl" "subl.Subl"
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <window_title> <launch_command> <window_class>"
   exit 1
 fi
 
-window_class="$1"
+window_title="$1"
 launch_command="$2"
+window_class="$3"
 
 active_win_id=$(xdotool getactivewindow)
 
@@ -17,8 +21,18 @@ active_win_class=$(xprop -id $active_win_id | grep 'WM_CLASS(STRING)' | awk -F '
 # echo $active_win_class > /tmp/foo.txt
 
 # Check if the active window matches the given class
-if [[ "$active_win_class" == "$window_class" ]]; then
+if [[ "$active_win_class" == "$window_title" ]]; then
   xdotool windowminimize $active_win_id
 else
-  $launch_command &
+  existing_win_id=$(wmctrl -lx | grep "$window_class" | awk '{print $1}' | head -n 1)
+  
+  # Check if the app is already running
+  if [ -z "$existing_win_id" ]; then
+    # Launch the app if not running
+    $launch_command &
+  else
+    # Activate the existing app window
+    wmctrl -i -a $existing_win_id
+  fi
+
 fi
