6f41335927689bad598d358089bd24c1e37d25f9
Thiago Silva <rthiago@gmail.com>
2024-06-13 13:38:06 -0300

    Fix snapshot link script

--- scripts/executable_link_latest_snapshot.sh
+++ scripts/executable_link_latest_snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LATEST_ROOT="$(snapper -c root list | grep timeline | tail -n1 | awk -F\| '{print $1}' | tr -d ' ')"
-LATEST_HOME="$(snapper -c home list | grep timeline | tail -n1 | awk -F\| '{print $1}' | tr -d ' ')"
+LATEST_ROOT="$(snapper -c root list | grep timeline | tail -n1 | awk '{print $1}' | tr -d ' ')"
+LATEST_HOME="$(snapper -c home list | grep timeline | tail -n1 | awk '{print $1}' | tr -d ' ')"
 
 ln -sfn /.snapshots/$LATEST_ROOT /.snapshots/latest
 ln -sfn /home/.snapshots/$LATEST_HOME /home/.snapshots/latest
