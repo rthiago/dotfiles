3c7e1f8ac66702fd8201eeb773b8a96699a2ce5d
Thiago Silva <rthiago@gmail.com>
2024-06-26 09:21:51 -0300

    Improve bitcoin quote script

--- scripts/executable_bitcoin-quote.sh
+++ scripts/executable_bitcoin-quote.sh
@@ -1,1 +1,1 @@
-echo -n "₿ " ; curl -s rate.sx/1BTC | cut -d . -f 1 | rev | fold -w3 | paste -sd'.' - | rev
+echo -n "₿ " ; result=$(curl -s --fail rate.sx/1BTC) || echo "failed" ; echo $result | cut -d . -f 1 | rev | fold -w3 | paste -sd'.' - | rev
