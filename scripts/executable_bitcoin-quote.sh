f7e95f03bdaafde586ae62166311dec132c5838d
Thiago Silva <rthiago@gmail.com>
2025-04-28 10:40:35 -0300

    Minor changes

--- scripts/executable_bitcoin-quote.sh
+++ scripts/executable_bitcoin-quote.sh
@@ -1,1 +1,1 @@
-echo -n "₿ " ; result=$(curl -s "https://api.coindesk.com/v1/bpi/currentprice/usd.json" | jq -r '.bpi.USD.rate_float *100|round/100') || echo "failed" ; echo $result
+echo -n "₿ " ; result=$(curl -s "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD" | jq -r '.USD *100|round/100') || echo "failed" ; echo $result
