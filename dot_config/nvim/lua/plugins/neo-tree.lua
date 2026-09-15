c5037d550ee29cf61aa7d61397f07ee2fe7ab7d3
Thiago Silva <rthiago@gmail.com>
2026-09-15 11:25:02 -0300

    Move Neo-tree sidebar right and widen it

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      position = "right",
+      width = 50,
+    },
     filesystem = {
       filtered_items = {
         visible = true,
